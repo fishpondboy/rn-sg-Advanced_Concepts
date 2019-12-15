f25041e5f6cdb521e2d7613b6b1249456b82a181
fishpondboy <jethro.kapantow@gmail.com>
2019-12-16 04:18:11 +0800

    #61
    Generating Access Codes

--- one-time-password/functions/request_one_time_password.js
+++ one-time-password/functions/request_one_time_password.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const admin = require('firebase-admin');
 const twilio = require('./twilio');
 
@@ -14,11 +15,25 @@ module.exports = function(req, res) {
     .then(userRecord => {
       const code = Math.floor(Math.random() * 8999 + 1000);
 
-      twilio.messages.create({
-        body: 'Your code is' + code,
-        to: phone,
-        from: '+16625984291'
-      });
+      twilio.messages.create(
+        {
+          body: 'Your code is' + code,
+          to: phone,
+          from: '+16625984291'
+        },
+        err => {
+          if (err) {
+            return res.status(422).send(err);
+          }
+
+          admin
+            .database()
+            .ref('users/' + phone)
+            .update({ code: code, codeValid: true }, () => {
+              res.send({ success: true });
+            });
+        }
+      );
     })
     .catch(err => {
       res.status(422).send({ error: err });
